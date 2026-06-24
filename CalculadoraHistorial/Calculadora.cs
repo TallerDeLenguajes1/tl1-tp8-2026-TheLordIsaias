6feb50fc7cd95cc8862451f5dabd94d3a307bcb7
TheLordIsaias <miruizselman@gmail.com>
2026-06-23 22:55:32 -0300

    TP Terminado

---
+++ CalculadoraHistorial/Calculadora.cs
@@ -0,0 +1,133 @@
+﻿using System.Security.Cryptography;
+
+namespace EspacioCalculadora{
+    public class Calculadora
+    {
+        private double dato;
+        private List<Operacion> historial;
+        public Calculadora(double dato)
+        {
+            this.dato = dato;
+            this.historial = new List<Operacion>();
+        }
+        public double Resultado
+        {
+            get => dato; 
+        }
+        public void Sumar(double termino)
+        {
+            Operacion operacion = new Operacion(dato);
+            operacion.NuevoValor = termino;
+            operacion.TipoDeOperacion = TipoOperacion.Suma;
+            historial.Add(operacion);
+            dato += termino;
+        }
+        public void Restar(double termino)
+        {
+            Operacion operacion = new Operacion(dato);
+            operacion.NuevoValor = termino;
+            operacion.TipoDeOperacion = TipoOperacion.Resto;
+            historial.Add(operacion);
+            dato -= termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            Operacion operacion = new Operacion(dato);
+            operacion.NuevoValor = termino;
+            operacion.TipoDeOperacion = TipoOperacion.Multiplicacion;
+            historial.Add(operacion);
+            dato *= termino;
+        }
+        public void Dividir(double termino)
+        {
+            Operacion operacion = new Operacion(dato);
+            operacion.NuevoValor = termino;
+            operacion.TipoDeOperacion = TipoOperacion.Division;
+            historial.Add(operacion);
+            dato /= termino;
+        }
+        public void Limpiar()
+        {
+            Operacion operacion = new Operacion(dato);
+            operacion.NuevoValor = 0;
+            operacion.TipoDeOperacion = TipoOperacion.Limpiar;
+            historial.Add(operacion);
+            dato = 0;
+        }
+        public List<Operacion> Historial
+        {
+            get
+            {
+                return historial;
+            }
+        }
+    }
+    public enum TipoOperacion
+    {
+        Suma,
+        Resto,
+        Multiplicacion,
+        Division,
+        Limpiar
+    }
+    public class Operacion
+    {
+        private double resultadoAnterior;
+        public double ResultadoAnterior
+        {
+            get
+            {
+                return resultadoAnterior;
+            }
+            set
+            {
+                resultadoAnterior = value;
+            }
+        }
+        private double nuevoValor;
+        public double NuevoValor
+        {
+            get
+            {
+                return nuevoValor;
+            }
+            set
+            {
+                nuevoValor = value;
+            }
+        }
+        public TipoOperacion TipoDeOperacion{get; set;}
+        public double Resultado()
+        {
+            double resultado = 0;
+            switch (TipoDeOperacion)
+            {
+                case TipoOperacion.Suma:
+                    resultado = resultadoAnterior + nuevoValor;
+                    break;
+                case TipoOperacion.Resto:
+                    resultado = resultadoAnterior - nuevoValor;
+                    break;
+                case TipoOperacion.Multiplicacion:
+                    resultado = resultadoAnterior * nuevoValor;
+                    break;
+                case TipoOperacion.Division:
+                    resultado = resultadoAnterior / nuevoValor;
+                    break;
+                case TipoOperacion.Limpiar:
+                    resultadoAnterior = 0;
+                    nuevoValor = 0;
+                    break;
+            }
+
+            return resultado;
+            
+        }
+        public Operacion(double resultadoAnterior)
+        {
+            this.resultadoAnterior = resultadoAnterior;
+            this.nuevoValor = 0;
+            this.TipoDeOperacion = TipoOperacion.Limpiar;
+        }
+    }
+}
---
+++ CalculadoraHistorial/Program.cs
@@ -0,0 +1,64 @@
+﻿using EspacioCalculadora;
+
+Calculadora calc = new Calculadora(25);
+
+Console.WriteLine($"Se creo una nueva instancia de la clase Calculadora con el valor {calc.Resultado}");
+
+bool bandera = true;
+int opcion;
+double termino;
+bool estado;
+
+do
+{
+    Console.WriteLine("\n¿Que operacion desea realizar?\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n0. Ninguna/Salir");
+    estado = int.TryParse(Console.ReadLine(), out opcion);
+    if(estado && opcion != 0)
+    {
+        Console.WriteLine("Ingrese un termino para realizar el calculo");
+        estado = double.TryParse(Console.ReadLine(), out termino);
+        if (estado)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    calc.Sumar(termino);
+                    break;
+                case 2:
+                    calc.Restar(termino);
+                    break;
+                case 3:
+                    calc.Multiplicar(termino);
+                    break;
+                case 4:
+                    calc.Dividir(termino);
+                    break;
+                default:
+                    Console.WriteLine("Hubo un error realizando la operacion, no se realizaron cambios");
+                    break;
+            }
+            Console.WriteLine($"El resultado de la operacion hecha fue {calc.Resultado}");
+        } 
+        else
+        {
+            Console.WriteLine("El numero ingresado no es valido, no se realizaron cambios");
+        }
+    }
+    else
+    {
+        if (!estado)
+        {
+            Console.WriteLine("La opcion elegida no es correcta, no se realizaron cambios");
+        }
+        else
+        {
+            calc.Limpiar();
+            bandera = false;
+        }
+    }
+}while(bandera);
+
+foreach (var operacion in calc.Historial)
+{
+    Console.WriteLine($"Operacion: {operacion.TipoDeOperacion}");
+}
